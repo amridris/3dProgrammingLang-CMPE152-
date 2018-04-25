02fa26d205427dcf48b22156ce516af43b51798f
JacobJones <jacob.o.jones@gmail.com>
2018-04-24 19:19:21 -0700

    added init_var

---
+++ java/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    private static class envirnoment
+    {
+        class point
+        {
+            private int x, y, z;
+            public point() { this(0,0,0); }
+            public point(int argx, int argy, int argz)
+            {
+                
+
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
