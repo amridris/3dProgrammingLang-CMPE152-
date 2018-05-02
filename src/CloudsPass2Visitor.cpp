e1390fefa9130a66ad79bd73080bcc3b27d76e7b
Jacob Jennings <Jacob@Jacobs-MacBook-Pro.local>
2018-05-02 12:26:42 -0700

    started work on pass2visitor

--- src/CloudsPass2Visitor.cpp
+++ src/CloudsPass2Visitor.cpp
@@ -17,3 +17,107 @@ CloudsPass2Visitor::CloudsPass2Visitor(ostream& j_file)
 }
 
 CloudsPass2Visitor::~CloudsPass2Visitor() {}
+
+antlrcpp::Any CloudsPass2Visitor::visitProgram(CloudsParser::ProgramContext *ctx)
+{
+    auto value = visitCHildren(ctx);
+    return value;
+}
+
+antlrcpp::Any CloudsPass2Visitor::visitHeader(CloudsParser::HeaderContext *ctx)
+{
+    program_name = ctx->ID()->toString();
+    return visitChildren(ctx);
+}
+
+antlrcpp::Any CloudsPass2Visitor::visitStat(CloudsParser::StatContext *ctx)
+{
+    j_file << endl << "; " + ctx->getText() << endl << endl;
+
+    return visitChildren(ctx);
+}
+
+antlrcpp::Any CloudsPass2Visitor::visitAssignment_stmt(CloudsParser::Assignment_stmtContext *ctx)
+{
+    auto value = visit(ctx->expr());
+
+    string type_indicator =
+                  (ctx->expr()->type == Predefined::integer_type) ? "I"
+                : (ctx->expr()->type == Predefined::real_type)    ? "F"
+                :                                                   "?";
+
+    // Emit a field put instruction.
+    j_file << "\tputstatic\t" << program_name
+           << "/" << ctx->variable()->ID()->toString()
+           << " " << type_indicator << endl;
+
+    return value;
+}
+
+antlrcpp::Any CloudsPass2Visitor::visitaddSubExpr(CloudsParser::AddSubExprContext *ctx)
+{
+    auto value = visitChildren(ctx);
+
+    TypeSpec *type1 = ctx->expr(0)->type;
+    TypeSpec *type2 = ctx->expr(1)->type;
+
+    bool integer_mode =    (type1 == Predefined::integer_type)
+                        && (type2 == Predefined::integer_type);
+    bool real_mode    =    (type1 == Predefined::real_type)
+                        && (type2 == Predefined::real_type);
+
+    string op = ctx->add_sub_op()->getText();
+    string opcode;
+
+    if (op == "+")
+    {
+        opcode = integer_mode ? "iadd"
+               : real_mode    ? "fadd"
+               :                "????";
+    }
+    else
+    {
+        opcode = integer_mode ? "isub"
+               : real_mode    ? "fsub"
+               :                "????";
+    }
+
+    // Emit an add or subtract instruction.
+    j_file << "\t" << opcode << endl;
+
+    return value;
+}
+
+antlrcpp::Any CloudsPass2Visitor::visitmulDivExpr(CloudsParser::MulDivExprContext *ctx)
+{
+    auto value = visitChildren(ctx);
+
+    TypeSpec *type1 = ctx->expr(0)->type;
+    TypeSpec *type2 = ctx->expr(1)->type;
+
+    bool integer_mode =    (type1 == Predefined::integer_type)
+                        && (type2 == Predefined::integer_type);
+    bool real_mode    =    (type1 == Predefined::real_type)
+                        && (type2 == Predefined::real_type);
+
+    string op = ctx->mul_div_op()->getText();
+    string opcode;
+
+    if (op == "+")
+    {
+        opcode = integer_mode ? "imul"
+               : real_mode    ? "fmul"
+               :                "????";
+    }
+    else
+    {
+        opcode = integer_mode ? "idiv"
+               : real_mode    ? "fdiv"
+               :                "????";
+    }
+
+    // Emit an add or subtract instruction.
+    j_file << "\t" << opcode << endl;
+
+    return value;
+}
